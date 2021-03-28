4dcc891e790513b5683828a51bede694ed63ba52
Corneliu Duplachi <cornel.duplachi@gmail.com>
2021-03-28 14:21:38 +0300

    fix: args for `execute` command

--- src/commands/interaction/execute.ts
+++ src/commands/interaction/execute.ts
@@ -1,6 +1,6 @@
 import { Driver } from '../../Driver';
 
-export async function execute(this: Driver, script: string, ...args: any[]): Promise<any> {
+export async function execute(this: Driver, script: string, args: any[]): Promise<any> {
   const [component, method] = script.split(':');
 
   if (!this.roku[component]?.[method]) {
