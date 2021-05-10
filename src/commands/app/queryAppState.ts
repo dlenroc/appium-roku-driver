8890658951976da3bb57b014871c961a72012db6
Corneliu Duplachi <cornel.duplachi@gmail.com>
2021-05-10 20:09:39 +0300

    fix: background state in `queryAppState`

--- src/commands/app/queryAppState.ts
+++ src/commands/app/queryAppState.ts
@@ -3,13 +3,13 @@ import { Driver } from '../../Driver';
 export async function queryAppState(this: Driver, id: string): Promise<number> {
   const activeApp = await this.roku.ecp.queryActiveApp();
 
-  // running in background
-  if (activeApp.screensaver) {
-    return 3;
-  }
-
-  // running in foreground
   if (activeApp.app['id'] == id) {
+    // running in background
+    if (activeApp.screensaver) {
+      return 3;
+    }
+
+    // running in foreground
     return 4;
   }
 
