da5f5497e684e772cbba31878fce575a6842ee20
Corneliu Duplachi <cornel.duplachi@gmail.com>
2021-10-17 18:14:07 +0300

    feat: support for `link text` and `partial link text` selector strategies

--- src/helpers/getSelector.ts
+++ src/helpers/getSelector.ts
@@ -10,10 +10,16 @@ export function getSelector(this: Driver, strategy: string, selector?: string): 
   switch (strategy) {
     case 'id':
       const tag = this.roku.document.context === 'ECP' ? 'name' : 'id';
-      return ['css selector', `[${tag}="${cssEscape(selector)}"]`];
+      return ['css selector', `[${tag}=${cssEscape(selector, { wrap: true })}]`];
 
     case 'tag name':
-      return ['css selector', cssEscape(selector)];
+      return ['css selector', cssEscape(selector, { isIdentifier: true })];
+
+    case 'link text':
+      return ['css selector', `[text=${cssEscape(selector, { wrap: true })}]`];
+
+    case 'partial link text':
+      return ['css selector', `[text*=${cssEscape(selector, { wrap: true })}]`];
 
     case 'xpath':
     case 'css selector':
