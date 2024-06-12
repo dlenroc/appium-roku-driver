42f759722bcf37d582bf9c6cc32764e96753ea6f
Corneliu Duplachi <cornel.duplachi@gmail.com>
2024-06-12 13:17:22 +0300

    feat!: foreground app with `activateApp`

--- src/commands/activateApp.ts
+++ src/commands/activateApp.ts
@@ -1,13 +1,14 @@
 import { errors } from '@appium/base-driver';
 import * as ecp from '@dlenroc/roku-ecp';
 import type { Driver } from '../Driver.ts';
+import * as appiumUtils from '../helpers/appium.js';
 
 export async function activateApp(
   this: Driver,
   appId: string,
   options?: unknown
 ): Promise<void> {
-  options ??= { odc_clear_registry: false };
+  options ??= {};
 
   if (typeof options !== 'object' || Array.isArray(options)) {
     throw new errors.InvalidArgumentError('Launch arguments must be an object');
@@ -16,5 +17,21 @@ export async function activateApp(
   await this.sdk.ecp.launch({
     appId: appId as ecp.AppId,
     params: options as Record<string, unknown>,
+  });
+
+  let screensaverDismissed = false;
+
+  await appiumUtils.retrying({
+    timeout: 1e4,
+    validate: (state, error) => !error && state === 4,
+    command: async () => {
+      const state = await this.queryAppState(appId);
+      if (!screensaverDismissed && state === 3) {
+        screensaverDismissed = true;
+        await this.sdk.ecp.keypress({ key: 'Enter' });
+      }
+
+      return state;
+    },
   });
 }
