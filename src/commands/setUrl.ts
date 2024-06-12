05d67a6a9ab35aef0a0e5b1f6ddb147907d9747b
Corneliu Duplachi <cornel.duplachi@gmail.com>
2024-06-12 13:41:08 +0300

    reuse `activateApp` in `setUrl`

--- src/commands/setUrl.ts
+++ src/commands/setUrl.ts
@@ -1,22 +1,17 @@
 import { errors } from '@appium/base-driver';
-import type { AppId } from '@dlenroc/roku-ecp';
 import { URL } from 'node:url';
 import type { Driver } from '../Driver.ts';
 
 export async function setUrl(this: Driver, url: string): Promise<void> {
   const { host, pathname, searchParams } = new URL(url);
-  const app = pathname.slice(1) as AppId;
-
-  let params: Record<string, string> = {};
-  for (const [key, value] of searchParams) {
-    params[key] = value;
-  }
+  const app = pathname.slice(1);
+  const params = Object.fromEntries(searchParams);
 
   switch (host) {
     case 'launch':
-      return await this.sdk.ecp.launch({ appId: app, params });
+      return this.activateApp(app, params);
     case 'input':
-      return await this.sdk.ecp.input(params);
+      return this.sdk.ecp.input(params);
     default:
       throw new errors.InvalidArgumentError('Unsupported URL format');
   }
